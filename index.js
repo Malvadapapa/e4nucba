1c223534ad3a501c536c42b701938661666441c0
MalvadaPapa <cristian.v62@gmail.com>
2022-10-06 16:05:53 -0300

    commit

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 
 const searchedPokemon = async () => {
 const respPokemon = await fetchPokemons()
+if(!selectedPokemon.value){
+    showError(selectedPokemon.value)
+} else{
 console.log(respPokemon)
-
+let mapedTypes = respPokemon.types.map((tipos) => {return tipos.type.name}).join(" ,")
 let pokeObjeto = {
     id:respPokemon.id,
     name:respPokemon.name,
-    altura:`${respPokemon.height/10}`
-
+    altura:`${respPokemon.height/10} mts`,
+    peso:`${respPokemon.weight/10} kg`,
+    tipos: mapedTypes,
+    img: `${respPokemon.sprites.other.home.front_default}`
 }
-console.log(pokeObjeto)
-return respPokemon
+renderPokemon(pokeObjeto)
+}
+}
+
+const renderPokemon = (pokemonObjet) => {
+renderCardSection.innerHTML = ` 
+<div class="printedCard">
+   <div class="imgContainer">
+        <img src="${pokemonObjet.img}" alt=""> 
+        <h3>${pokemonObjet.name}</h3>
+    </div>
+<div class="pokedataContainer">
+    <ol>
+        <li>Tipo: ${pokemonObjet.tipos}</li>
+        <li>Peso: ${pokemonObjet.peso}</li>
+        <li>Altura: ${pokemonObjet.altura}</li>
+    </ol>
+</div>
+</div>
+`
 }
 
 const init = () =>{
